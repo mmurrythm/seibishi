33ea24b9f9c63a0b1025393a954186ecc72423e1
Muhammed Maruf-Ur-Rashid <275057643+mmurrythm@users.noreply.github.com>
2026-09-01 00:35:46 +0600

    Refactor main.c for Sokoban game initialization

--- main.c
+++ main.c
@@ -1,26 +1,80 @@
-#include "raylib.h"
+#include"raylib.h"
+#include <stdlib.h>
+#include <time.h>
+
+#define BOARD_SIZE 10
+#define TILE_SIZE 32
+#define TILE_TYPES 4
+const char tile_chars[TILE_TYPES] = {' ', '#', '.', '$'}; // Empty, Wall, Goal, Box
+
+char board[BOARD_SIZE][BOARD_SIZE] ;
+
+char random_tile()
+{
+    int r = GetRandomValue(0, TILE_TYPES - 1);
+    return tile_chars[r];
+}
+
+void init_board()
+{
+    for (int y = 0; y < BOARD_SIZE; y++)
+    {
+        for (int x = 0; x < BOARD_SIZE; x++)
+        {
+            board[y][x] = random_tile();
+        }
+    }
+}
 
 int main(void)
 {
-    InitWindow(800, 600, "Sokoban");
+    const int screenWidth = 800;
+    const int screenHeight = 600;
+
+    InitWindow(screenWidth, screenHeight, "Sokoban Game");
     SetTargetFPS(60);
+    SetRandomSeed((unsigned int)time(NULL)); // Seed the random number generator
+    init_board();
 
     while (!WindowShouldClose())
     {
-        // 1. UPDATE
-        // Read input and change the game state
 
-        // 2. DRAW
+        // Update game logic here
         BeginDrawing();
-
         ClearBackground(RAYWHITE);
 
-        // Draw the game here
+        for (int y = 0; y < BOARD_SIZE; y++)
+        {
+            for (int x = 0; x < BOARD_SIZE; x++)
+            {
+                Rectangle rect =
+                {
+                    x * TILE_SIZE,
+                    y * TILE_SIZE,
+                    TILE_SIZE,
+                    TILE_SIZE
+                };
+
+                DrawRectangleLinesEx(rect, 1, BLACK);
+
+                DrawTextEx(
+                    GetFontDefault(),
+                    TextFormat("%c", board[y][x]),
+                    (Vector2)
+                {
+                    rect.x + 12, rect.y + 6
+                },
+                24,
+                1,
+                BLACK
+                );
+                // DrawText(&board[y][x], x * TILE_SIZE, y * TILE_SIZE, 20, BLACK);
+            }
+        }
 
         EndDrawing();
     }
 
     CloseWindow();
-
     return 0;
 }
