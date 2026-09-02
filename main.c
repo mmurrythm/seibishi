1504324be42a381365aa424398b18d1301a9053b
Muhammed Maruf-Ur-Rashid <275057643+mmurrythm@users.noreply.github.com>
2026-09-03 00:20:45 +0600

    Refactor board initialization and level handling
    Updated board initialization to include multiple levels and corrected variable names for clarity.

--- main.c
+++ main.c
@@ -12,57 +12,212 @@ const char tileTypes[TILE_TYPES] = {' ', '#', '.', '$'}; // Empty, Wall, Goal, B
 char board[BOARD_SIZE][BOARD_SIZE];
 char goals[BOARD_SIZE][BOARD_SIZE];
 
-char randomtile()
+void init_Board(int levelNum)
 {
-    int r = GetRandomValue(0, TILE_TYPES - 1);
-    return tileTypes[r];
-}
-
-void init_Board(int levelnum)
-{
-    //from line 24 to 62, we defined layout
+    //from line 24 to 214, we defined layout
     char level1[BOARD_SIZE][BOARD_SIZE] =
-        {
-            "################",
-            "#              #",
-            "#              #",
-            "#              #",
-            "#              #",
-            "#              #",
-            "#              #",
-            "#              #",
-            "#              #",
-            "#              #",
-            "#              #",
-            "#              #",
-            "#              #",
-            "#              #",
-            "#              #",
-            "################"
-            
-        };
+    {
+        "################",
+        "#              #",
+        "#   $      .   #",
+        "#      ##      #",
+        "#              #",
+        "#   ###        #",
+        "#       .      #",
+        "#        $     #",
+        "#              #",
+        "#      ###     #",
+        "#              #",
+        "#   #          #",
+        "#              #",
+        "#              #",
+        "#              #",
+        "################"
+    };
+
     char level2[BOARD_SIZE][BOARD_SIZE] =
-        {
-            "################",
-            "#              #",
-            "#              #",
-            "#              #",
-            "#              #",
-            "#              #",
-            "#              #",
-            "#              #",
-            "#              #",
-            "#              #",
-            "#              #",
-            "#              #",
-            "#              #",
-            "#              #",
-            "#              #",
-            "################"
-};
+    {
+        "################",
+        "#              #",
+        "#  $           #",
+        "#  ###   ###   #",
+        "#      .       #",
+        "#      ###     #",
+        "#              #",
+        "#   ##         #",
+        "#   ##    .    #",
+        "#        $     #",
+        "#              #",
+        "#      ###     #",
+        "#              #",
+        "#              #",
+        "#              #",
+        "################"
+    };
+    char level3[BOARD_SIZE][BOARD_SIZE] =
+    {
+        "################",
+        "#       #      #",
+        "#  $    #  .   #",
+        "#       #      #",
+        "###   #######  #",
+        "#              #",
+        "#   .          #",
+        "#       ###    #",
+        "#       ###    #",
+        "#          $   #",
+        "#              #",
+        "#  #######     #",
+        "#              #",
+        "#       #      #",
+        "#              #",
+        "################"
+    };
+    char level4[BOARD_SIZE][BOARD_SIZE] =
+    {
+        "################",
+        "#              #",
+        "#   ####       #",
+        "#   #  .#      #",
+        "#   #   #      #",
+        "#   #   ####   #",
+        "#   #      .   #",
+        "#   ######     #",
+        "#              #",
+        "#      $       #",
+        "#              #",
+        "#       $      #",
+        "#              #",
+        "#              #",
+        "#              #",
+        "################"
+    };
+    char level5[BOARD_SIZE][BOARD_SIZE] =
+    {
+        "################",
+        "#     #        #",
+        "#  $  #   .    #",
+        "#     #        #",
+        "# ### #####    #",
+        "#   #         #",
+        "#   #   .    #",
+        "#   #       #",
+        "#   ##### ###  #",
+        "#       $      #",
+        "#              #",
+        "#  ########    #",
+        "#              #",
+        "#              #",
+        "#              #",
+        "################"
+    };
+    char level6[BOARD_SIZE][BOARD_SIZE] =
+    {
+        "################",
+        "#              #",
+        "#   #######    #",
+        "#   #     #    #",
+        "# $ #  .  #    #",
+        "#   #     #### #",
+        "#   ###        #",
+        "#       .      #",
+        "#       ####   #",
+        "#           $  #",
+        "#   ######     #",
+        "#              #",
+        "#              #",
+        "#              #",
+        "#              #",
+        "################"
+    };
+    char level7[BOARD_SIZE][BOARD_SIZE] =
+    {
+        "################",
+        "#              #",
+        "#  $      # .  #",
+        "#          #   #",
+        "#  ####    #   #",
+        "#     #    ### #",
+        "#  .  #        #",
+        "#     ####     #",
+        "#              #",
+        "#     $        #",
+        "#   ########   #",
+        "#              #",
+        "#              #",
+        "#              #",
+        "#              #",
+        "################"
+    };
+    char level8[BOARD_SIZE][BOARD_SIZE] =
+    {
+        "################",
+        "#      #       #",
+        "#  $   #   .   #",
+        "#      #       #",
+        "#  ##########  #",
+        "#              #",
+        "#    .         #",
+        "#      ####    #",
+        "#          $   #",
+        "#      ####    #",
+        "#              #",
+        "#  ##########  #",
+        "#              #",
+        "#      #       #",
+        "#              #",
+        "################"
+    };
+    char level9[BOARD_SIZE][BOARD_SIZE] =
+    {
+        "################",
+        "#              #",
+        "#   ###    .   #",
+        "#   #          #",
+        "# $ #   ####   #",
+        "#   #          #",
+        "#   #######    #",
+        "#       .      #",
+        "#              #",
+        "#       ####   #",
+        "#          $   #",
+        "#              #",
+        "#   #######    #",
+        "#              #",
+        "#              #",
+        "################"
+    };
+    char level10[BOARD_SIZE][BOARD_SIZE] =
+    {
+        "################",
+        "#              #",
+        "#  $    #      #",
+        "#       #  .   #",
+        "#   ### #      #",
+        "#       ####   #",
+        "#              #",
+        "#   .      $   #",
+        "#              #",
+        "#    #####     #",
+        "#              #",
+        "#  #######     #",
+        "#              #",
+        "#       #      #",
+        "#              #",
+        "################"
+    };
     // tile screen position = board offset + grid position × tile size
 
-    char (*level)[BOARD_SIZE] = (levelnum == 1) ? level1 : level2;
+    char (*level)[BOARD_SIZE] =
+    (levelNum == 1) ? level1 :
+    (levelNum == 2) ? level2 :
+    (levelNum == 3) ? level3 :
+    (levelNum == 4) ? level4 :
+    (levelNum == 5) ? level5 :
+    (levelNum == 6) ? level6 :
+    (levelNum == 7) ? level7 :
+    (levelNum == 8) ? level8 :
+    (levelNum == 9) ? level9 :level10;
 
     for (int y = 0; y < BOARD_SIZE; y++)
     {
@@ -80,14 +235,14 @@ int main(void)
 
     InitWindow(screenwidth, screenheight, "Sokoban Game");
     SetTargetFPS(60);
-    SetRandomSeed((unsigned int)time(NULL)); // Seed the random number generator
-    init_Board(1);
+    int levelNum = 1;
+
+    init_Board(levelNum);
 
     //game variables
     int playerX = 2, playerY = 2;
-    int levelnum = 1;
-
     int movescount = 0;
+    
     int boardWidth = BOARD_SIZE * TILE_SIZE, boardHeight = BOARD_SIZE * TILE_SIZE;
 
     int boardoffsetX = (screenwidth - boardWidth) / 2;
@@ -108,13 +263,13 @@ int main(void)
 
     // while game loop
     int gamewon = 0;
-    while (!WindowShouldClose()) 
+    while (!WindowShouldClose())
     {
 
         // Update game logic here
         if (IsKeyPressed(KEY_R))
         {
-            init_Board(levelnum);
+            init_Board(levelNum);
             playerX = 2;
             playerY = 2;
             movescount = 0;
@@ -196,12 +351,12 @@ int main(void)
             for (int x = 0; x < BOARD_SIZE; x++)
             {
                 Rectangle rect =
-                    {
-                        boardoffsetX + x * TILE_SIZE,
-                        boardoffsetY + y * TILE_SIZE,
-                        TILE_SIZE,
-                        TILE_SIZE
-                    };
+                {
+                    boardoffsetX + x * TILE_SIZE,
+                    boardoffsetY + y * TILE_SIZE,
+                    TILE_SIZE,
+                    TILE_SIZE
+                };
 
                 // tile content drawing
                 if (board[y][x] == '#')
@@ -230,7 +385,10 @@ int main(void)
 
                 DrawRectangleLinesEx(rect, 1, BLACK);
 
-                DrawTextEx(GetFontDefault(), TextFormat("%c", board[y][x]), (Vector2){rect.x + 12, rect.y + 6}, 24, 1, BLACK);
+                DrawTextEx(GetFontDefault(), TextFormat("%c", board[y][x]), (Vector2)
+                {
+                    rect.x + 12, rect.y + 6
+                }, 24, 1, BLACK);
                 // DrawText(&board[y][x], x * TILE_SIZE, y * TILE_SIZE, 20, BLACK);
             }
         }
@@ -243,15 +401,15 @@ int main(void)
 
         DrawText(TextFormat("MOVES: %d", movescount), margin, screenheight - fontSize - margin, fontSize, BLACK);
         DrawText(TextFormat("GOALS: %d / %d", goalCount, totalcount), margin, screenheight - fontSize - margin - 25, fontSize, BLACK);
-        
+
         if(gamewon == 1)
         {
             DrawRectangle(250, 05, 295, 45, RED);
             DrawText("YOU WIN!", 300, 10, 40, GREEN); // center of the screen (400,400)
         }
-  
+
         EndDrawing();
-    } 
+    }
 
     CloseWindow();
     return 0;
